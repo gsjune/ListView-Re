17f4c015366b2e596b05855a51debb4441d359fc
Hongchul Kim <gsjune@naver.com>
2017-07-27 23:25:31 +0900

    adapterview package -> AdapterViewExamActivity

---
+++ src/main/java/com/hckim/listviewre/adapterview/AdapterViewExamActivity.java
@@ -0,0 +1,15 @@
+package com.hckim.listviewre.adapterview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hckim.listviewre.R;
+
+public class AdapterViewExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapter_view_exam);
+    }
+}
