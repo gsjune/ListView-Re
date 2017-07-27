15985d15b15fd0d464a45abfdadf7c06bd164162
Hongchul Kim <gsjune@naver.com>
2017-07-28 01:14:00 +0900

    adapter 패키지에 java class PeopleAdapter 만들며 super class에 BaseAdapter 표기 및 선택 -> implement methods

---
+++ src/main/java/com/hckim/listviewre/adapterview/PeopleAdapter.java
@@ -0,0 +1,32 @@
+package com.hckim.listviewre.adapterview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by K on 2017-07-28.
+ */
+
+public class PeopleAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
